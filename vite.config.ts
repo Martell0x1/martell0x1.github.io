313a47227010ab3318eb5ca839c1ff1790683096
Marwan Mohamed Zein <marawanzein222@gmail.com>
2026-01-22 07:01:28 +0200

    Fixing vite config and deploy.yml

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/system-forge/",
+  base: "/",
   server: {
     host: "::",
     port: 8080,
